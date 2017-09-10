7ae8fd0fc6dfaac106d6d01002f0668aebc5a459
jesus <chuyelregio5@yahoo.com>
2017-09-09 21:29:57 -0700

    finished CRUD

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,7 +24,83 @@ Route::get('/create', function () {
 
     $user = User::find(1);
 
-    $role = new Role(['name' => 'Admin']);
+    $role = new Role(['name' => 'Author']);
 
     $user->roles()->save($role);
 });
+
+
+Route::get('/read', function () {
+    
+    $user = User::find(1);
+
+    foreach ($user->roles as $role) {
+        echo ($role)->content;
+    }
+    
+});
+
+Route::get('/update', function () {
+    $user = User::findOrFail(1);
+
+    if($user->has('roles')) {
+        foreach ($user->roles as $role) {
+            if($role->name == 'Admin') {
+                $role->name = "subscriber";
+                $role->save();
+            }
+        }
+    }
+});
+
+
+Route::get('/delete', function () {
+    $user = User::findOrFail(1);
+
+    foreach($user->roles as $role) {
+
+        $role->whereId(1)->delete();
+
+    }
+});
+
+
+Route::get('/attach', function () {
+
+    $user = User::findOrFail(1);
+
+    $user->roles()->attach(2);
+
+});
+
+
+Route::get('/detach', function () {
+
+    $user = User::findOrFail(1);
+
+    $user->roles()->detach();
+
+});
+
+
+Route::get('/sync', function () {
+
+    $user = User::findOrFail(1);
+
+    $user->roles()->sync([4,5]);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
